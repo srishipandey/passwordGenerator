53dbc4d136696f98eb02918abbec49a20d0ef493
Rishi P <rishipandey.programming@gmail.com>
2021-04-14 20:40:01 +0530

    .

--- script.js
+++ script.js
@@ -2,28 +2,47 @@
 // .fromCharCode() -- is a method
 // Browser Character Set
 
+// DOM
+const resultEl = document.getElementById("result");
+const lengthEl = document.getElementById("length");
+const uppercaseEl = document.getElementById("uppercase");
+const lowercaseEl = document.getElementById("lowercase");
+const numbersEl = document.getElementById("numbers");
+const symbolsEl = document.getElementById("symbols");
+const generateEl = document.getElementById("generate");
+const clipboardEl = document.getElementById("clipboard");
+
 const randomFunc = {
-  lower: getRandomLower,
-  upper: getRandomUpper,
-  numbers: getRandomNumbers,
-  symbols: getRandomSymbols,
+	lower: getRandomLower,
+	upper: getRandomUpper,
+	numbers: getRandomNumbers,
+	symbols: getRandomSymbols,
 };
 
+// Events
+	generateEl.addEventListener("click", () => {
+        const length = +lengthEl.value
+        const hasUpper = uppercaseEl.checked
+        const hasLower = lowercaseEl.checked
+        const hasNumber = numbersEl.checked
+        const hasSymbols = symbolsEl.checked
+	});
+
+
+// Generator Functions
 function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
 function getRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
 
 function getRandomNumbers() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 48);
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 48);
 }
 
 function getRandomSymbols() {
-  const symbols = "!@#$%^&*(){}[]=<>/,.";
-  return symbols[Math.floor(Math.random() * symbols.length)];
+	const symbols = "!@#$%^&*(){}[]=<>/,.";
+	return symbols[Math.floor(Math.random() * symbols.length)];
 }
-
-// console.log(symbols);
